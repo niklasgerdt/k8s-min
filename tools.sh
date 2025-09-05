e2636bc0b9b16e8ed8440556f5aeadc35fd691f7
Niklas Gerdt <niklas.gerdt@gmail.com>
2025-09-05 15:01:21 +0300

    tools listing update

--- tools.sh
+++ tools.sh
@@ -1,5 +1,22 @@
 sudo apt update
 sudo apt install build-essential
 sudo snap install docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 
